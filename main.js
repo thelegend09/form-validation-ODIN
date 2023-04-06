43ef50148c0000ec82aa115282bfb289681c30ef
the legend <legendre.oli@gmail.com>
2023-04-06 12:08:01 -0400

    refactor email and country verification. Now, both have an event listener on 'focuseout' that runs first and only once. After, another listener is triggered on 'input'.

--- main.js
+++ main.js
@@ -201,55 +201,88 @@ function verifyCountry(inputCountry) {
     return allCountries.includes(inputCountry);
 }
 
-let countryDiv = document.querySelector("div.country");
-let countryInput = document.querySelector(".country input");
-let countryMessage = document.querySelector(".country .message");
+function fieldVerifCountry() {
+    let countryDiv = document.querySelector("div.country");
+    let countryInput = document.querySelector(".country input");
+    let countryMessage = document.querySelector(".country .message");
+    let countryCounter = 0;
 
-countryDiv.addEventListener("focusout", (e) => {
-    // let words = e.target.value;
-    let country = capitalizeFirstLetter(e.target.value);
-    countryInput.value = country;
+    countryDiv.addEventListener(
+        "focusout",
+        (e) => {
+            countryCounter++;
+            validateField(e.target.value);
+        },
+        { once: true }
+    );
 
-    if (country === "") {
-        undoValidation(countryDiv);
-        countryMessage.textContent = "";
-    } else if (!verifyCountry(country)) {
-        toggleClasses(countryDiv, "invalid");
-        countryMessage.textContent =
-            "You might have misspeled that country name.";
-    } else if (verifyCountry(country)) {
-        toggleClasses(countryDiv, "valid");
-        countryMessage.textContent = "";
+    countryDiv.addEventListener("input", (e) => {
+        if (countryCounter === 0) return;
+        validateField(e.target.value);
+    });
+
+    function validateField(value) {
+        // let words = e.target.value;
+        let country = capitalizeFirstLetter(value);
+        countryInput.value = country;
+
+        if (country === "") {
+            undoValidation(countryDiv);
+            countryMessage.textContent = "";
+        } else if (!verifyCountry(country)) {
+            toggleClasses(countryDiv, "invalid");
+            countryMessage.textContent =
+                "You might have misspeled that country name.";
+        } else if (verifyCountry(country)) {
+            toggleClasses(countryDiv, "valid");
+            countryMessage.textContent = "";
+        }
     }
-});
 
-function capitalizeFirstLetter(word) {
-    return word.charAt(0).toUpperCase() + word.slice(1);
+    function capitalizeFirstLetter(word) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    }
 }
 
 //  EMAIL
-let emailDiv = document.querySelector("div.email");
-let emailMessage = document.querySelector(".email .message");
 
-emailDiv.addEventListener("focusout", (e) => {
-    if (e.target.value === "") {
-        undoValidation(emailDiv);
-        emailMessage.textContent = "";
-    } else if (!emailValidation(e.target.value)) {
-        toggleClasses(emailDiv, "invalid");
-        emailMessage.textContent = "Please enter a valid email.";
-    } else {
-        toggleClasses(emailDiv, "valid");
+function fieldVerifEmail() {
+    let emailDiv = document.querySelector("div.email");
+    let emailMessage = document.querySelector(".email .message");
+    let counter = 0;
+    emailDiv.addEventListener(
+        "focusout",
+        (e) => {
+            emailCounter++;
+            validateField(e.target.value);
+        },
+        { once: true }
+    );
 
-        emailMessage.textContent = "";
+    emailDiv.addEventListener("input", (e) => {
+        if (counter === 0) return;
+        validateField(e.target.value);
+    });
+
+    function validateField(value) {
+        if (value === "") {
+            undoValidation(emailDiv);
+            emailMessage.textContent = "";
+        } else if (!emailValidation(value)) {
+            toggleClasses(emailDiv, "invalid");
+            emailMessage.textContent = "Please enter a valid email.";
+        } else {
+            toggleClasses(emailDiv, "valid");
+
+            emailMessage.textContent = "";
+        }
     }
-});
-
-function emailValidation(email) {
-    if (email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-        return true;
-    } else {
-        return false;
+    function emailValidation(email) {
+        if (email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
@@ -278,3 +311,8 @@ function toggleClasses(element, makeValidorInvalid) {
         element.classList.add("valid");
     }
 }
+
+(function init() {
+    fieldVerifEmail();
+    fieldVerifCountry();
+})();
